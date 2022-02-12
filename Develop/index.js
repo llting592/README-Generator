2b24da296e57adebd96740ff0faf56ce7376ba46
llting592 <llting592@gmail.com>
2022-02-12 16:24:11 -0500

    second commit, ready for deployment

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,59 @@
-// TODO: Include packages needed for this application
+function init () {
 
-// TODO: Create an array of questions for user input
-const questions = [];
+    const inquirer = require('inquirer');
+    const fs = require('fs'); 
+    const generate = require('./utils/generateMarkdown.js');
+    const path = require('path');
+    
+    inquirer
+      .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a description of your project',
+          },
+          {
+            type: 'input',
+            name: 'install',
+            message: 'Please give a brief instruction on how to install',
+          },
+          {
+            type: 'input',
+            name: 'usage',
+            message: 'Please give a short summary of how to use this',
+          },
+          {
+            type: 'input',
+            name: 'tests',
+            message: 'Please give a short description of any tests written',
+          },
+          {
+              type: 'input',
+              name: 'github',
+              message: 'Please enter your github username',
+            },
+          {
+              type: 'input',
+              name: 'email',
+              message: 'Please enter your email address',
+          },
+            {
+              type: 'checkbox',
+              name: 'licenses',
+              message: 'Please choose select a license (only choose one)',
+              choices: ['MIT', 'Apache 2.0', 'None'],
+            },
+      ])
+      .then((response) => {
+        return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+      });
+    }
+    
+    init();
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+ 
